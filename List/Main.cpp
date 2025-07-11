04d0ed8c9fa7633a81e13a99d7c2906ac6a5a263
SuadoCowboy <luccacsg@gmail.com>
2025-07-11 14:05:47 -0300

    Improved List

--- List/Main.cpp
+++ List/Main.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
+#include <cstdlib>
+#include <stdint.h>
+
+#ifdef _MSC_VER
+#include <windows.h>
+#endif
 
 void* operator new(size_t size) {
+#ifdef _MSC_VER
+    char buf[64];
+    sprintf_s(buf, "Allocated %zu bytes\n", size);
+    OutputDebugStringA(buf);
+#else
     std::cout << "Allocated " << size << " bytes\n";
+#endif
     return malloc(size);
 }
 
+#ifdef _MSC_VER
+void* operator new[](size_t size) {
+    std::cout << "Allocated array of " << size << " bytes\n";
+    return malloc(size);
+}
+
+void* operator new(size_t size, const std::nothrow_t&) noexcept {
+    std::cout << "Allocated (nothrow) " << size << " bytes\n";
+    return malloc(size);
+}
+
+void operator delete(void* pObject, size_t size) noexcept {
+    free(pObject);
+    std::cout << "Deallocated " << size << " bytes\n";
+}
+
+void operator delete[](void* pObject) noexcept {
+    free(pObject);
+    std::cout << "Deallocated an array object\n";
+}
+
+void operator delete[](void* pObject, size_t size) noexcept {
+    free(pObject);
+    std::cout << "Deallocated array of " << size << " bytes\n";
+}
+#else
 void operator delete(void* pObject) {
     free(pObject);
     std::cout << "Deallocated an object\n";
 }
+#endif
 
-template<typename T>
+template<typename T, typename Size = size_t>
 class List {
 public:
     List() {}
@@ -20,49 +59,50 @@ public:
     }
     
     void clear() {
-        for (size_t i = 0; i < size; ++i) {
-            delete pData[i];
-        }
+        for (Size i = 0; i < size; ++i)
+            delete pArray[i];
+
         size = 0;
-        delete[] pData;
-        pData = nullptr;
+        delete[] pArray;
+        pArray = nullptr;
     }
 
     void push_back(const T& object) {
-        T** pNewData = new T*[size+1];
-        for (size_t i = 0; i < size; ++i)
-            pNewData[i] = pData[i];
+        T** pNewArray = new T*[size+1];
+        for (Size i = 0; i < size; ++i)
+            pNewArray[i] = pArray[i];
 
-        delete[] pData;
-        pData = pNewData;
+        if (pArray)
+            delete[] pArray;
+        pArray = pNewArray;
 
-        pData[size] = new T(object);
+        pArray[size] = new T(object);
         ++size;
     }
     
-    T* operator[](size_t index) {
+    T* operator[](Size index) {
         if (index >= size)
             return nullptr;
         else
-            return pData[index];
+            return pArray[index];
     }
 
-    T** pData = nullptr;
-    size_t size = 0;
+    T** pArray = nullptr;
+    Size size = 0;
 };
 
 int main()
 {
-    List<int> integers;
+    List<int, uint8_t> integers;
     integers.push_back(10);
-    integers.push_back(25);
+    integers.push_back(-25);
     integers.push_back(69420);
     integers.push_back(420569);
     
-    for (size_t i = 0; i < integers.size; ++i) {
+    for (decltype(integers.size) i = 0; i < integers.size; ++i) {
         std::cout << *integers[i] << " ";
     }
-    
+
     std::cout << "\nHeap: " << integers.size*sizeof(int) << "\n";
 
     return 0;
